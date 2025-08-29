fe415ba20a844f2cc2a27768d24c1906aaff7e48
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-08-29 17:55:58 +0530

    urls and tailwind css

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'tailwind',
+    'theme'
 ]
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
