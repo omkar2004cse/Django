32ac53428526032d8bb853b846b105702b88bc2f
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-11-03 10:16:58 +0530

    django project

--- POST_X/Tweet/urls.py
+++ POST_X/Tweet/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('',views.tweet,name='tweet'),
 
 ]
--- POST_X/Tweet/views.py
+++ POST_X/Tweet/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def tweet(request):
-    return render(request,'index.html')
+    return render(request,'app.html')
